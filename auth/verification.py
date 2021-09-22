6db368404873d8c177b80186468255078bac74ee
Alex Stulen <ookno16@gmail.com>
2021-09-22 19:44:58 +0300

    Update, feature: (added new field for returns and updated Note) for auth/verification.py

--- auth/verification.py
+++ auth/verification.py
@@ -12,11 +12,27 @@ def Verification():
 		'last_four_ssn': 'last four ssn id numbers' # required
 	}
 
-	# Note:
-	# Key Token = None for python or null for JS if authorization is failed
+	"""
+	Note:
+		Key Token = None for python or null for JS if authorization is failed
+		if authorization is failed: response status = 200 and response will have info about field which field was wrong.
+	
+		Key role, nurse and facility = null if authorization is failed.
+		if role equal 'facility' -> field 'nurse' equal null.
+		if role equal 'nurse' -> field 'facility' equal null.
+	"""
+
 	returns = {
 		'Token': '...', # Authorization Token
 		'verified': 'boolean field', # True - if verification is successful. False - if verification is not successful.
+		'role': 'nurse or facility. equal null if authorization is failed',
+		'nurse': {
+			'pk': 'primary key for nurse',
+			'is_active_nurse': 'boolean field'
+		},
+		'facility': {
+			'pk': 'primary key for facility'
+		},
         'employee_id_is_valid': 'boolean field',
         'last_four_ssn_is_valid': 'boolean field',
 	}
