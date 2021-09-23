e37f0a3718a229a737cde1c856cac8139cd31a94
Alex Stulen <ookno16@gmail.com>
2021-09-23 16:59:04 +0300

    Update: added ability to get shift info by pk

---
+++ get_post_obj/shift/get/get_shift.py
@@ -0,0 +1,132 @@
+# Get detail shift info
+
+
+def Get_Shift():
+
+	"""
+		Note:
+			to get information about a shift, you do not need to specify the fields for a get request. 
+			It is enough to specify the primary key of the shift in the address bar instead of <pk>.
+	"""
+	api_utl = 'https://firstchoicenurses.com/api/v1/mobile/obj/shift/<pk>/'
+
+	method = 'GET'
+	headers = REQUIRED_HEADERS_FOR_TOKEN_AUTH
+
+	get_fields = 'special get fields are not needed'
+
+	returns = {
+		'shift': {
+			'hospital':{
+				'name': '<type: str>',
+				'pk': 'primary key'
+			},
+			'comments': '<type: str>',
+			'nurse': {
+				'pk': 'primary key',
+			},
+			'cancelled': '<type: bool>',
+			'certified': '<type: bool>',
+			'certified_by': {
+				'user':{
+					'pk': 'primary key',
+					'username': '<type: str>'
+				}
+			},
+			'certified_on': {
+				'year': '<type: int>',
+				'month': '<type: int>',
+				'day': '<type: int>',
+				'time': '<type: str>'
+			},
+			'start_datetime': {
+				'year': '<type: int>',
+				'month': '<type: int>',
+				'day': '<type: int>',
+				'time': '<type: str>'
+			},
+			'stop_datetime': {
+				'year': '<type: int>',
+				'month': '<type: int>',
+				'day': '<type: int>',
+				'time': '<type: str>'
+			},
+			'daylight_saving_time': '<type: str>',
+			'duration': {
+				'hours': '<type: int>',
+				'minutes': '<type: int>',
+				'seconds': '<type: int>'
+			}
+			'regular_duration': {
+				'hours': '<type: int>',
+				'minutes': '<type: int>',
+				'seconds': '<type: int>'
+			},
+			'break_duration': {
+				'hours': '<type: int>',
+				'minutes': '<type: int>',
+				'seconds': '<type: int>'
+			},
+			'overtime_duration': {
+				'hours': '<type: int>',
+				'minutes': '<type: int>',
+				'seconds': '<type: int>'
+			},
+			'holiday_duration': {
+				'hours': '<type: int>',
+				'minutes': '<type: int>',
+				'seconds': '<type: int>'
+			},
+			'client': '<type: str>',
+			'department': '<type: str>',
+			'date': {
+				'year': '<type: int>',
+				'month': '<type: int>',
+				'day': '<type: int>'
+			},
+			'classification': '<type: str>',
+			'ordered_by': '<type: str>',
+			'received': {
+				'year': '<type: int>',
+				'month': '<type: int>',
+				'day': '<type: int>',
+				'time': '<type: str>'
+			},
+			'received_by': 'received by <type: str>',
+			'shift': '<type: str>',
+			'start_time': '<type: int>',
+			'stop_time': '<type: int>',
+			'regular_hours': '<type: float>',
+			'break_hours': '<type: float>',
+			'overtime_hours': '<type: float>',
+			'holiday_hours': '<type: float>',
+			'travel': '<type: float>',
+			'check_date': {
+				'year': '<type: int>',
+				'month': '<type: int>',
+				'day': '<type: int>'
+			},
+			'check_number': '<type: int>',
+			'check_amount': '<type: float>',
+			'invoice_number': '<type: str>',
+			'invoice_date': {
+				'year': '<type: int>',
+				'month': '<type: int>',
+				'day': '<type: int>'
+			},
+			'invoice_amount': '<type: float>',
+			'ok_to_bill': '<type: bool>',
+			'ok_overtime': '<type: bool>',
+			'exported': '<type: bool>',
+			'site': '<type: str>',
+			'paychex_id': '<type: int>',
+			'worker': '<type: str>',
+			'keyfield': '<type: str>',
+			'timestamp': {
+				'year': '<type: int>',
+				'month': '<type: int>',
+				'day': '<type: int>',
+				'time': '<type: str>'
+			}
+		}
+	}
