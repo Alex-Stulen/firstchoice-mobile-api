25c796c894606081d2990e1fb7802b04031c4592
Alex Stulen <ookno16@gmail.com>
2021-09-23 14:24:01 +0300

    Update: updated api.py. Added example_dev_api_url value

--- api.py
+++ api.py
@@ -4,9 +4,12 @@
 # all responses will be serialized to the JSON format
 
 site_url = 'https://firstchoicenurses.com'
+
+# for development use dev_site_url
 dev_site_url = 'https://dev.firstchoicenurses.com'
 api_url = '/api/v1/mobile/'
 example_api_url = 'https://firstchoicenurses.com/api/v1/mobile/.../'
+example_dev_api_url = 'https://dev.firstchoicenurses.com/api/v1/mobile/.../'
 
 RETURN_STATUSES = {
 	'200': 'ok',
