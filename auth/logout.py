a65e186afd0878499b6077c3aa0f8e94137b8f3b
Alex Stulen <ookno16@gmail.com>
2021-10-04 22:34:20 +0300

    Feature: added logout ability

---
+++ auth/logout.py
@@ -0,0 +1,19 @@
+# Logout ability
+
+def Logout():
+	api_url = 'https://firstchoicenurses.com/api/v1/mobile/auth/logout/'
+
+	method = 'GET'
+	headers = REQUIRED_HEADERS_FOR_TOKEN_AUTH
+
+	get_fields = 'special get fields are not needed'
+
+	# Note:
+	#	if logout is success:
+	#		returns status code 200.
+	#	else:
+	#		returns status code 401.
+	#
+	# special data in response missing
+	returns = {}
+
