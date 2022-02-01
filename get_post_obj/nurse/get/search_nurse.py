12ba69bbe37fe136c86db391119e40abb81ca642
Alex Stulen <ookno16@gmail.com>
2022-02-01 21:18:00 +0200

    Update: updated search nurse. Added pagination. Changed get field from fname/lname to text

--- get_post_obj/nurse/get/search_nurse.py
+++ get_post_obj/nurse/get/search_nurse.py
@@ -1,10 +1,15 @@
 # Search Nurse by fname or lname fields
-'https://dev.firstchoicenurses.com/api/v1/mobile/obj/nurse/search?fname=...&lname=...'
+'https://dev.firstchoicenurses.com/api/v1/mobile/obj/nurse/search?text=Alex+Test&page=1'
 method = 'GET'
 headers = REQUIRED_HEADERS_FOR_TOKEN_AUTH
 get_fields = {
-	'fname': <type: string. Optional>,
-	'lname': <type: string. Optional>
+	'text': <type: string. Required>,
+	'page': <type: int. Optional. Default equal 1>
 }
 
-returns = 'returns array with nurse objects'
+
+returns = {
+    "total_pages": <type: int>,
+    "current_page": <type: int>,
+    "data": <type: array. Array with nurses objects>
+}
--- urls.py
+++ urls.py
@@ -367,11 +367,16 @@ returns = {
 method = 'GET'
 headers = REQUIRED_HEADERS_FOR_TOKEN_AUTH
 get_fields = {
-	'fname': <type: string. Optional>,
-	'lname': <type: string. Optional>
+	'text': <type: string. Required>,
+	'page': <type: int. Optional. Default equal 1>
 }
 
-returns = 'returns array with nurse objects'
+
+returns = {
+    "total_pages": <type: int>,
+    "current_page": <type: int>,
+    "data": <type: array. Array with nurses objects>
+}
 
 """===================================================================="""
 
